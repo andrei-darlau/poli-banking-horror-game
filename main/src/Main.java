449ee86c3f88934e18077908c8256719120b215e
andrei-darlau <andreidarlau04@gmail.com>
2024-11-22 00:22:25 +0200

    displayed a frame

---
+++ main/src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(1000, 1000);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Ai card la BCR?");
+
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
